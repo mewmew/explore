b2fb6ea673bc9ea9c457f5bee3bcca50eac0399f
Robin Eklind <rnd0x00@gmail.com>
2018-12-29 03:06:46 +0100

    cmd/explore: add highlighted LLVM IR output

--- cmd/explore/main.go
+++ cmd/explore/main.go
@@ -34,6 +34,9 @@ import (
 	"strings"
 	"text/template"
 
+	"github.com/alecthomas/chroma/formatters/html"
+	"github.com/alecthomas/chroma/lexers"
+	"github.com/alecthomas/chroma/styles"
 	"github.com/llir/llvm/asm"
 	"github.com/llir/llvm/ir"
 	"github.com/mewkiz/pkg/goutil"
@@ -191,6 +194,9 @@ func genVisualization(llPath string, f *ir.Function, dotDir, htmlDir string) err
 		if err := ioutil.WriteFile(htmlPath, htmlContent, 0644); err != nil {
 			return errors.WithStack(err)
 		}
+		if err := genLLVMHighlight(llPath, f, prim, step); err != nil {
+			return errors.WithStack(err)
+		}
 	}
 	return nil
 }
@@ -222,6 +228,87 @@ func genStep(llPath string, f *ir.Function, prim *primitive.Primitive, step, nst
 		return nil, errors.WithStack(err)
 	}
 	return buf.Bytes(), nil
+}
+
+// genLLVMHighlight generates a visualization in HTML format of the intermediate
+// step of the control flow analysis, highlighting the lines of the LLVM IR for
+// the corresponding basic blocks of the recovered high-level control flow
+// primitive.
+func genLLVMHighlight(llPath string, f *ir.Function, prim *primitive.Primitive, step int) error {
+	// Get Chroma LLVM IR lexer.
+	lexer := lexers.Get("llvm")
+	if lexer == nil {
+		lexer = lexers.Fallback
+	}
+	//lexer = chroma.Coalesce(lexer)
+	// Get Chrome Monokai style.
+	style := styles.Get("monokai")
+	if style == nil {
+		style = styles.Fallback
+	}
+	// Get Chroma HTML formatter.
+	// Line number ranges to highlight; 1-based line numbers, inclusive.
+	var highlightRanges [][2]int
+	for _, blockName := range prim.Nodes {
+		block, err := findBlock(f, blockName)
+		if err != nil {
+			return errors.WithStack(err)
+		}
+		highlightRange := lineRangeOfBlock(f, block)
+		highlightRanges = append(highlightRanges, highlightRange)
+	}
+	formatter := html.New(
+		html.TabWidth(3),
+		html.WithLineNumbers(),
+		// TODO: re-enable once https://github.com/alecthomas/chroma/issues/211 is fixed.
+		//html.WithClasses(),
+		html.LineNumbersInTable(),
+		html.HighlightLines(highlightRanges),
+	)
+
+	// Write CSS.
+	llvmContent := &bytes.Buffer{}
+	llvmContent.WriteString("<!DOCTYPE html><html><head><style>")
+	if err := formatter.WriteCSS(llvmContent, style); err != nil {
+		return errors.WithStack(err)
+	}
+	iterator, err := lexer.Tokenise(nil, f.Def())
+	if err != nil {
+		return errors.WithStack(err)
+	}
+	llvmContent.WriteString("</style></head><body>")
+	if err := formatter.Format(llvmContent, style, iterator); err != nil {
+		return errors.WithStack(err)
+	}
+	llvmContent.WriteString("</body></html>")
+
+	//if err := quick.Highlight(llvmContent, f.Def(), "llvm", "html", "monokai"); err != nil {
+	//	return errors.WithStack(err)
+	//}
+	exploreDir := pathutil.TrimExt(llPath) + "_explore"
+	llvmHTMLName := fmt.Sprintf("llvm_%04d.html", step)
+	llvmHTMLPath := filepath.Join(exploreDir, llvmHTMLName)
+	dbg.Printf("creating %q", llvmHTMLPath)
+	if err := ioutil.WriteFile(llvmHTMLPath, llvmContent.Bytes(), 0644); err != nil {
+		return errors.WithStack(err)
+	}
+	return nil
+}
+
+// lineRangeOfBlock returns the line number range (1-based: [start, end]) of the
+// basic block in the given function.
+func lineRangeOfBlock(f *ir.Function, block *ir.BasicBlock) [2]int {
+	funcStr := f.Def()
+	blockStr := block.Def()
+	pos := strings.Index(funcStr, blockStr)
+	if pos == -1 {
+		panic(fmt.Errorf("unable to locate contents of basic block %v in contents of function %v", block.Ident(), f.Ident()))
+	}
+	before := funcStr[:pos]
+	start := 1 + strings.Count(before, "\n")
+	n := strings.Count(blockStr, "\n")
+	end := start + n
+	return [2]int{start, end}
 }
 
 // getDOTDir returns the control flow graphs directory based on the path of the
@@ -290,21 +377,13 @@ func parsePrims(dotDir, funcName string) ([]*primitive.Primitive, error) {
 	return prims, nil
 }
 
-/*
-// outputHTML outputs the given control flow analysis visualization in HTML
-// format.
-//
-// For a source file "foo.ll" containing the functions "bar" and "baz" the
-// following HTML files are produced:
-//
-//    foo_explore/bar.html
-//    foo_explore/baz.html
-func outputHTML(htmlContent []byte, funcName, htmlDir string) error {
-	htmlName := funcName + ".html"
-	htmlPath := filepath.Join(htmlDir, htmlName)
-	if err := ioutil.WriteFile(htmlPath, htmlContent, 0644); err != nil {
-		return errors.WithStack(err)
+// findBlock locates and returns the basic block with the specified name in the
+// given function.
+func findBlock(f *ir.Function, blockName string) (*ir.BasicBlock, error) {
+	for _, block := range f.Blocks {
+		if block.Name() == blockName {
+			return block, nil
+		}
 	}
-	return nil
+	return nil, errors.Errorf("unable to locate basic block %q in function %q", blockName, f.Name())
 }
-*/
